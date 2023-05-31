984f4c7e2556284430892a0955ae2f658153432c
fazlay <rfazlay21@gmail.com>
2023-05-31 17:27:25 +0600

    first commit

---
+++ junior-dev-bug/src/Hooks/useGlobal.js
@@ -0,0 +1,33 @@
+import axios from "axios";
+import { useState } from "react";
+import countryCode from "../Features/Checkout/Data/countryCode.json";
+const useGlobal = () => {
+  const [open, setOpen] = useState(false);
+  const [mbCode, setMbCode] = useState(countryCode[15]);
+  const [totalPrice, setTotalPrice] = useState(0);
+
+  const toggleModal = () => setOpen(!open);
+
+  const getPayment = (body) => {
+    axios.post(`${process.env.REACT_APP_SERVER_URL}/api/bkash/createPayment`, { ...body, totalPrice })
+    .then((data) => {
+   
+      const bkashURL = data.data
+      console.log(bkashURL)
+      window.open(bkashURL);
+    })
+    .catch(error =>
+      console.log(error)
+    )
+  }
+  return {
+    toggleModal,
+    open,
+    setMbCode,
+    mbCode,
+    getPayment,
+    totalPrice,
+    setTotalPrice,
+  };
+};
+export default useGlobal;
